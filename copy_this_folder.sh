a2ed13f2ffe1feedaa43636d81ad1193c43f5726
tomerOko <tomeroko2@gmail.com>
2022-04-18 22:52:29 +0300

    add

--- copy_this_folder.sh
+++
@@ -1,6 +0,0 @@
-origin_directory=${PWD##*/} # set the name of the current directory to variable 'origin_directory'
-cd ../
-sudo chown -R $USER:$(id -gn $USER) ./* # give permmisions in order to be able to adit the files
-mkdir $1
-cp -R ./$origin_directory/* ./$1
-code ./$1
